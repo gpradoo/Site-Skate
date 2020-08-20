a1dfe56b8f599d3c9137debc9a3d8cf6411c22df
gpradocode <gpradocode@gmail.com>
2020-08-20 17:58:41 -0300

    GP alterações JavaScript, fontes, responbilidade etc etc

--- _js/script.js
+++ _js/script.js
@@ -1,5 +1,27 @@
+var visibilidade = true;
+
 function vejaMais(){
-    document.getElementById('ver-mais').style.display = "inline";
+    document.getElementById('ver-mais').style.display = "inline";   
 }
 
+function Close(){
+    document.getElementById('ver-mais').style.display = "none";
+}
+function OpenClose(){
+    if (visibilidade){
+        document.getElementById('ver-mais').style.display="none";
+        visibilidade = false;
+    }else{
+        document.getElementById('ver-mais').style.display="inline";
+        visibilidade = true;
+    }
+}
+/*document.getElementById("ver-mais").onclick() {myFunction()};
 
+function myFunction(){
+    document.getElementById("novas").classList.toggle("show")
+
+})
+
+document.getElementById('ver-mais').onclick( {vejaMais()}); 
+*/
